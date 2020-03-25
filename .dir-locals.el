edac7d9a80e062e364451d981a55e7683e4838ec
MoHKale <mohkalsin@gmail.com>
2020-03-25 14:11:09 +0000

    dir-locals: display yaml files with line numbers

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,3 @@
 ;;; Directory Local Variables
-(("sh/" . ((mode . shell-script)))
- ("bash/" . ((mode . shell-script)))
- ("zshell/" . ((mode . shell-script)))
- ("shortcuts/" . ((mode . shell-script)))
- ("aliases/" . ((mode . conf-space)))
- ("tmux/scripts/" ((mode . conf-space))))
+
+((yaml-mode . ((eval . (display-line-numbers-mode)))))
