71a0d8667e3a8e6454f9a84583ebefa971c37fc0
Mohsin Kaleem <mohkalsin@gmail.com>
2020-06-05 17:37:41 +0100

    paths: move ~/programming/scripts to ~/.local/bin

--- scripts/private/test_ycmd.sh
+++
@@ -1,1 +0,0 @@
-curl -LO 'https://raw.githubusercontent.com/ycm-core/ycmd/master/ycmd/default_settings.json' 2>/dev/null && python3 -u "${YCMD_PATH:-${HOME}/.vim/plugged/YouCompleteMe/third_party/ycmd/ycmd}" --options_file default_settings.json
