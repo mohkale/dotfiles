9045f06e5506e9dba910c8c40de08fc67d287c37
Mohsin Kaleem <mohkale@kisara.moe>
2020-12-14 15:44:21 +0000

    (bin): Add library directory
    Finally got tired of having to re-define code that's shared between python scripts.
    Now I put shared-code into /bin/lib/.

---
+++ bin/lib/python/terminal.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+import io
+import sys
+import logging
+
+def open_tty() -> io.FileIO:
+    """Open your current terminals tty and return it as a file descriptor.
+    """
+    try:
+        tty = os.ttyname(2)
+    except: tty = '/dev/tty'
+
+    try:
+        return io.TextIOWrapper(io.FileIO(os.open(tty, os.O_NOCTTY | os.O_RDWR), "r+"))
+    except:
+        logging.fatal('Failed to open tty: %s', tty)
+        sys.exit(1)
+
