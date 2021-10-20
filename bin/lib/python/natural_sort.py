3971b95f8e22fe9522e7abf319c75ad2213c4d46
Mohsin Kaleem <mohkale@kisara.moe>
2021-10-20 19:41:19 +0100

    (bin): Add blockify
    A little script to bulk convert images to blocks. This just crops images
    into same dimension squares with support for left aligning, right
    aligning, centring and more. By default it recursively traverses all
    image files in the current working directory and blocks them into a
    .blocks sub-directory.

--- bin/lib/python/natural_sort.py
+++ bin/lib/python/natural_sort.py
@@ -4,7 +4,7 @@ Sort collections naturally.
 import re
 
 key = lambda key: [int(chunk) if chunk.isdigit() else chunk.lower()
-              for chunk in re.split('([0-9]+)', key.path)]
+              for chunk in re.split('([0-9]+)', key)]
 
 def natural_sort(it):
     """Sort and return it naturally.
