8acdd4fae7df45b27fc462a3b42beaad3d79a52d
Mohsin Kaleem <mohkale@kisara.moe>
2021-11-27 15:31:26 +0000

    (brave): Add startpage
    A clean and minimalist start-page for various browser. As of now only
    supported on brave.

--- prog/browsers/brave/.config.sh
+++ prog/browsers/brave/.config.sh
@@ -1,2 +1,9 @@
 packages yay:brave-bin                          \
          choco:brave
+
+# There's no automated way to install unofficial chrome extensions, so if
+# you want the start-page you're going to have to open brave://extensions,
+# enable developer mode, load-unpacked, then navigate to ./startpage and
+# load it.
+sync-submodule startpage
+link -f startpage.json:startpage/config.json
