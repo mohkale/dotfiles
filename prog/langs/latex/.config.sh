3b2d0be29884d6336d7412987860e56f78d2d102
Mohsin Kaleem <mohkale@kisara.moe>
2021-12-25 16:15:38 +0000

    (misc): Update
    + Add a magit shell script
    + Cleanup jctl aliases and add traceroute alias
    + Update (and revert) some submodules

--- prog/langs/latex/.config.sh
+++ prog/langs/latex/.config.sh
@@ -1,1 +1,1 @@
-packages yay:texlive-core,texlive-latexextra,texlive-lang,texlive-fontsextra
+packages yay:texlive-core,texlive-latexextra,texlive-lang,texlive-fontsextra,latex-pgfplots,texlive-science
