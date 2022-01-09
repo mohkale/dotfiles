e5b30f7f142d6933adac2b617ce03753bf0da216
Mohsin Kaleem <mohkale@kisara.moe>
2022-01-09 21:25:03 +0000

    (hledger): Add commands and tmuxp configs
    + Commands contains a script to auto gpg decrypt my ledger files and to
    auto compile them into a beancount ledger for fava.

--- prog/hledger/.config.sh
+++ prog/hledger/.config.sh
@@ -1,4 +1,6 @@
 link ledger2beancount.yml:"$XDG_CONFIG_HOME/ledger2beancount/config.yaml"
+link-to "$XDG_DOCUMENTS_DIR/ledger/bin" ./cmds/*
+link-to "$XDG_CONFIG_HOME/tmuxp/" ./tmux/*
 
 packages yay:hledger-bin,ledger2beancount
 packages pip:fava
