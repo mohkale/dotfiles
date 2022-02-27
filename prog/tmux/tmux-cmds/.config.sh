13c0da9a39c2df3993420b6010dfb4849b326a1d
Mohsin Kaleem <mohkale@kisara.moe>
2022-02-27 13:17:18 +0000

    (tmux): Add extracto to fuzzy yank words

--- prog/tmux/tmux-cmds/.config.sh
+++ prog/tmux/tmux-cmds/.config.sh
@@ -1,1 +1,8 @@
-link "$XDG_CONFIG_HOME/tmux/cmds"/{attach-session,manage-windows,run-binding,run-command,jump-marked,setup-fzf}
+link                                            \
+  "$XDG_CONFIG_HOME/tmux/cmds"/attach-session   \
+  "$XDG_CONFIG_HOME/tmux/cmds"/extracto         \
+  "$XDG_CONFIG_HOME/tmux/cmds"/jump-marked      \
+  "$XDG_CONFIG_HOME/tmux/cmds"/manage-windows   \
+  "$XDG_CONFIG_HOME/tmux/cmds"/run-binding      \
+  "$XDG_CONFIG_HOME/tmux/cmds"/run-command      \
+  "$XDG_CONFIG_HOME/tmux/cmds"/setup-fzf
