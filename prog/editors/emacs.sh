a02c599d0e0754bfaae574fa62253c34d907e973
Mohsin Kaleem <mohkale@kisara.moe>
2022-03-06 17:24:45 +0000

    (misc): Update
    + Add rpm extracter to bin/extract
    + Bump emacs submodule.

--- prog/editors/emacs.sh
+++ prog/editors/emacs.sh
@@ -1,4 +1,6 @@
 sync-submodule ./emacs
+
+# Note: If trying to build from source, see [[https://emacs.stackexchange.com/questions/59538/compile-emacs-from-feature-native-comp-gccemacs-branch-on-ubuntu][here]].
 packages apt:emacs \
          choco:emacs \
          yay:libgccjit,emacs-git
