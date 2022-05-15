1027923e6973f4219abf498ff93b601102bca1d7
Mohsin Kaleem <mohkale@kisara.moe>
2022-05-15 11:03:03 +0100

    misc: Update
    + Fix bug in docker images cwd resolution.
    + Remove unused jellyfin kodi configuration.
    + Add jellyfin tmuxp record.

--- prog/media/jellyfin/.config.sh
+++ prog/media/jellyfin/.config.sh
@@ -1,15 +1,6 @@
 link-to "$XDG_BIN_DIR" ./cmds/*
+link-to "$XDG_CONFIG_HOME/tmuxp/" tmux/*
 
 packages yay:jellyfin-media-player
 
 import ../../docker
-# run-cmd docker-compose -f jellyfin.yml build
-
-import ../kodi
-JELLYFIN_KODI_REPO=$XDG_CACHE_HOME/repository.jellyfin.kodi.zip
-if ! [ -e "$JELLYFIN_KODI_REPO" ]; then
-  run-cmd curl -L --output "$JELLYFIN_KODI_REPO" https://kodi.jellyfin.org/repository.jellyfin.kodi.zip
-
-  # https://jellyfin.org/docs/general/clients/kodi.html#general-use-devices-pcs-and-tablets
-  todo "Open KODI and install the jellyfin add-on browser at $JELLYFIN_KODI_REPO"
-fi
