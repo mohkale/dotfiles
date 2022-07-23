f5fe0e35a6f3aa496744c93fffd19f2ccf2824a2
Mohsin Kaleem <mohkale@kisara.moe>
2022-07-23 11:24:26 +0100

    misc: Update
    + Update Dropbox docs directory location.
    + Update jellyfin docker image to build local plugins.

--- prog/dropbox/.module.sh
+++ prog/dropbox/.module.sh
@@ -2,4 +2,4 @@ packages                                        \
   choco:dropbox                                 \
   yay:dropbox
 
-link -i ~/Dropbox/org/docs:"$XDG_DOCUMENTS_DIR/books"
+link -i ~/Dropbox/docs:"$XDG_DOCUMENTS_DIR/books"
