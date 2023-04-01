b0cc7f03151488fff953ebc80d4af84288668b6b
Mohsin Kaleem <mohkale@kisara.moe>
2023-04-01 20:01:48 +0100

    misc: Update

--- dist/linux/dotty.sh
+++ dist/linux/dotty.sh
@@ -6,7 +6,7 @@ if package yay; then
           ttf-symbola                           \
           noto-fonts                            \
           bdf-unifont                           \
-          ttf-nerd-fonts-symbols                \
+          ttf-nerd-fonts-symbols-2048-em-mono   \
           ttf-meslo                             \
           s-nail                                \
           moreutils                             \
