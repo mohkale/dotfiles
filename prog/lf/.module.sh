8b2d85b493d1e1403e38b8640da6be7ca16f53a5
Mohsin Kaleem <mohkale@kisara.moe>
2023-04-15 10:48:07 +0100

    lf: Add lf-tags command

--- prog/lf/.module.sh
+++ prog/lf/.module.sh
@@ -3,10 +3,13 @@ packages                                        \
   choco:lf                                      \
   go:github.com/gokcehan/lf
 
+clean -r "$XDG_CONFIG_HOME/lf"
+
 link "$XDG_CONFIG_HOME/lf/lfrc"
+link-to "$XDG_BIN_DIR" ./cmds/*
 link-to "$XDG_CONFIG_HOME/autoloads/cmds/" ./auto/*
 link-to "$XDG_CONFIG_HOME/lf/auto/" ./utils/*
-link-to "$XDG_CONFIG_HOME/lf/cmds/" ./cmds/*
+link-to "$XDG_CONFIG_HOME/lf/cmds/" ./lf-cmds/*
 
 link dolphin.desktop:"$XDG_DATA_HOME/kservices5/ServiceMenus/open-lf.desktop"
 
