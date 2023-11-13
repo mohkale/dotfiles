2a7d0da967d34121ba4bbc0bdea967e3e69d9804
Mohsin Kaleem <mohkale@kisara.moe>
2023-11-13 23:35:13 +0100

    notmuch: Split tagging and archiving in post-new to separate scripts

--- prog/mail/notmuch/hooks/.module.sh
+++ prog/mail/notmuch/hooks/.module.sh
@@ -1,4 +1,4 @@
-link "$XDG_CONFIG_HOME/notmuch/default/hooks/post-new"
+link "$XDG_CONFIG_HOME/notmuch/default/hooks/post-new"{,.tags,.archive}
 
 bots mail.server && link pre-new.server:"$XDG_CONFIG_HOME/notmuch/default/hooks/pre-new"
 bots mail.client && link pre-new.client:"$XDG_CONFIG_HOME/notmuch/default/hooks/pre-new"
