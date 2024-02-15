0ff2e4089e9f4900ce1f47c1379ef38013dce1f9
Mohsin Kaleem <mohkale@kisara.moe>
2024-02-15 21:29:13 +0000

    theme: Deprecate mullvad-vpn and nordvpn segments

--- bin/theme/.config.sh
+++ bin/theme/.config.sh
@@ -10,8 +10,6 @@ link                                            \
   "$XDG_BIN_DIR/docker"                         \
   "$XDG_BIN_DIR/github-notifications"           \
   "$XDG_BIN_DIR/mpd-playback"                   \
-  "$XDG_BIN_DIR/mullvad-vpn"                    \
-  "$XDG_BIN_DIR/nordvpn"                        \
   "$XDG_BIN_DIR/notmuch"                        \
   "$XDG_BIN_DIR/status-misc"                    \
   "$XDG_BIN_DIR/systemd"                        \
