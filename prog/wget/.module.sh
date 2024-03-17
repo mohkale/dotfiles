4b8a2d1e80d20f0c1ec9052569b5803b84077e75
Mohsin Kaleem <mohkale@kisara.moe>
2024-03-17 13:35:36 +0000

    wget: Use config file directly

--- prog/wget/.module.sh
+++ prog/wget/.module.sh
@@ -1,8 +1,3 @@
 packagex wget
 
-info "Generating WgetRC File"
-run-cmds <<-'EOF'
-dest="$XDG_CONFIG_HOME/wgetrc"
-cp -f wgetrc "$dest"
-sed -i -e 's ~ '"$HOME"' g' "$dest"
-EOF
+link "$XDG_CONFIG_HOME/wgetrc"
