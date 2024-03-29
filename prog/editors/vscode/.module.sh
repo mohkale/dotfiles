2c9ab53c69f574ff2565f8e3c35418285ed8e0a9
Mohsin Kaleem <mohkale@kisara.moe>
2024-03-29 20:53:09 +0000

    vscode: Revamp + add vim plugin

--- prog/editors/vscode/.module.sh
+++ prog/editors/vscode/.module.sh
@@ -5,6 +5,7 @@ packages                                        \
 link                                                \
   "$XDG_CONFIG_HOME/VSCodium/User/settings.json"    \
   "$XDG_CONFIG_HOME/VSCodium/User/keybindings.json"
+link "$XDG_CONFIG_HOME/VSCodium:$XDG_CONFIG_HOME/Code"
 
 info "Installing vscode extensions"
 run-cmds <<-"EOF"
@@ -12,6 +13,7 @@ sed -e 's/ *//g' -e 's/#.*$//' <<EOP |
   dustinsanders.an-old-hope-theme-vscode
   2gua.rainbow-brackets
   wayou.vscode-todo-highlight
+  vscodevim.vim
 EOP
 xargs -r -d '\n' -n1 codium --install-extension
 EOF
