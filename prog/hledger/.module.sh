3cb83e655ec43d1d059c4b38930a7d7e4a3f575f
Mohsin Kaleem <mohkale@kisara.moe>
2024-07-21 11:09:35 +0100

    hledger: Install cmds to global PATH

--- prog/hledger/.module.sh
+++ prog/hledger/.module.sh
@@ -1,5 +1,5 @@
 link ledger2beancount.yml:"$XDG_CONFIG_HOME/ledger2beancount/config.yaml"
-link-to "$XDG_DOCUMENTS_DIR/ledger/bin" ./cmds/*
+link-to "$XDG_BIN_DIR" ./cmds/*
 link-to "$XDG_CONFIG_HOME/tmuxp/" ./tmux/*
 
 packagex hledger ledger2beancount fava
