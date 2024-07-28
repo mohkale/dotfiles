5eb8b1a61d6128e02d6f3f232860c757b14ea40c
Mohsin Kaleem <mohkale@kisara.moe>
2024-07-28 17:42:30 +0100

    cron: Add config to force MAILTO self

--- prog/cron/.module.sh
+++ prog/cron/.module.sh
@@ -1,4 +1,10 @@
 link-to "$XDG_BIN_DIR" cmds/*
 
+makedir "$XDG_CONFIG_HOME/cron-user.d"
+info 'Generating mail-to-self cron record'
+run-cmds <<-'EOF'
+echo "MAILTO=$USER@localhost" > "$XDG_CONFIG_HOME/cron-user.d/mail-to-self.cron"
+EOF
+
 # Provides chronic.
 packagex chronic
