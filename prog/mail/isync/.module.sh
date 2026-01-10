436b036b47526f97fe799de918b05830275dadbe
Mohsin Kaleem <mohkale@kisara.moe>
2026-01-10 17:22:30 +0000

    isync: Source passwords from local files
    This is needed since I no longer install pass.

--- prog/mail/isync/.module.sh
+++ prog/mail/isync/.module.sh
@@ -1,5 +1,10 @@
 if bots mail.server; then
   link ~/.config/isync/mbsyncrc
+  makedir -m 700 ~/.config/isync/passwords/
+  makefile -m 600 \
+      ~/.config/isync/passwords/mohkalsin@gmail.com \
+      ~/.config/isync/passwords/mohkalex@gmail.com
+
   link-to "$XDG_BIN_DIR/" bin/*
 
   packages pacman:isync
