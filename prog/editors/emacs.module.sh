a4547359c4577594c402341af8e92dcabc120a60
Mohsin Kaleem <mohkale@kisara.moe>
2026-01-10 17:24:53 +0000

    emacs: Add module dependency on enchant

--- prog/editors/emacs.module.sh
+++ prog/editors/emacs.module.sh
@@ -1,3 +1,5 @@
+import -f enchant
+
 sync-submodule ./emacs
 
 # Note: If trying to build from source, see [[https://emacs.stackexchange.com/questions/59538/compile-emacs-from-feature-native-comp-gccemacs-branch-on-ubuntu][here]].
