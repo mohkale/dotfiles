89bf5393c36b8fdfbcf6eac4b2eef92f1a7d212e
Mohsin Kaleem <mohkale@kisara.moe>
2026-06-06 12:28:10 +0100

    lnxlink: Move to config template

--- prog/smarthome/lnxlink/.module.sh
+++ prog/smarthome/lnxlink/.module.sh
@@ -1,5 +1,7 @@
+if copy "config.yaml.in:$XDG_CONFIG_HOME/lnxlink/config.yaml"; then
+  info "Copied lnxlink template to $XDG_CONFIG_HOME/lnxlink/config.yaml"
+fi
 link \
-  "$XDG_CONFIG_HOME/lnxlink/config.yaml" \
   "$XDG_CONFIG_HOME/systemd/user/lnxlink.service" \
   "$XDG_CONFIG_HOME/lnxlink/lnxlink-screen-lock-inhibitor-daemon.py" \
   "$XDG_CONFIG_HOME/systemd/user/lnxlink-screen-lock-inhibitor-daemon.service"
